a2c4c1fe7bdfdf214a0011a5e01c13732acf0226
SilverB_Study <89564181+Eunbijou@users.noreply.github.com>
2022-03-26 21:36:57 +0900

    Add files via upload

---
+++ Java/project/pjtTest/src/pjtTest/EighteenthClass.java
@@ -0,0 +1,17 @@
+package pjtTest;
+
+public class EighteenthClass {
+
+	public static void main(String[] args) {
+		
+		// 배열과 선언과 초기화를 동시에
+		int[] arr2 = {10, 20, 30, 40, 50};
+		
+		System.out.println("arr2[0] : " + arr2[0]);
+		System.out.println("arr2[1] : " + arr2[1]);
+		System.out.println("arr2[2] : " + arr2[2]);
+		System.out.println("arr2[3] : " + arr2[3]);
+		System.out.println("arr2[4] : " + arr2[4]);
+		
+	}
+}
