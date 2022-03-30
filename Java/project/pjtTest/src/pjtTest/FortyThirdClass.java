d8f641c5a3d2e196d359fd7efa7c38c761b390ef
SilverB_Study <89564181+Eunbijou@users.noreply.github.com>
2022-03-31 03:40:10 +0900

    Update and rename FortiethThirdClass.java to FortyThirdClass.java

--- Java/project/pjtTest/src/pjtTest/FortyThirdClass.java
+++ Java/project/pjtTest/src/pjtTest/FortyThirdClass.java
@@ -1,20 +1,20 @@
 package pjtTest;
 
-public class FortiethThirdClass {
+public class FortyThirdClass {
 	
 	public int x;
 	public int y;
 
-	public FortiethThirdClass(String s, int[] iArr) {
+	public FortyThirdClass(String s, int[] iArr) {
 		
-		System.out.println("-- FortiethThirdClass() --");
+		System.out.println("-- FortyThirdClass() --");
 		
 		System.out.println("s -----> " + s);
 		System.out.println("iArr -----> " + iArr);
 		
 	}
 	
-	public FortiethThirdClass(int x, int y) {
+	public FortyThirdClass(int x, int y) {
 		
 		this.x = x;
 		this.y = y;
